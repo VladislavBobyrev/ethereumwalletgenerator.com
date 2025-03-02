519878115dfa3edca3a4b208fec35507d17b69a6
v.bobyrev <v.bobyrev@vk.team>
2025-03-02 23:59:04 +0300

    create branch for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,6 @@ export default defineConfig({
         port: 3000,
     },
     build: {
-        outDir: './',
-        emptyOutDir: false,
-        assetsDir: './src',
-        rollupOptions: {
-            output: {
-                entryFileNames: '[name].js',
-                chunkFileNames: '[name].js',
-                assetFileNames: '[name][extname]',
-            },
-        },
+        outDir: 'dist',
     },
-    base: 'ethereumwalletgenerator.com/',
 });
